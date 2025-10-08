f101cd30237161c3d340bd79a1847c90ee313a62
Govardhan Raju <gsurapur@gmail.com>
2025-10-08 21:42:46 +0400

    shell

--- roboshop.sh
+++ roboshop.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-
+#
 AMI_ID=ami-09c813fb71547fc4f
 SG_ID=sg-0caa92da7b80302bc
 
 for instance in $@
 do
-INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query Instances[0].InstanceId --output text)
 
-if [ $instance != "frontend" ]; then
-	IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-else
-    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-fi
+   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0caa92da7b80302bc --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    #get Private IP
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0caa92da7b80302bc --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    else
+        IP=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0caa92da7b80302bc --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].PublicIpAddress' --output text)
+    fi
 
-echo "$instance: $IP"
-
+    echo "$instance: $IP"
 done
