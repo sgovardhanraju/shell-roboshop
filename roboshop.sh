de6a0c9322e8464c997f7afd9bd7e144cd355f9b
Govardhan Raju <gsurapur@gmail.com>
2025-10-08 22:24:51 +0400

    shell

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 #
 AMI_ID=ami-09c813fb71547fc4f
 SG_ID=sg-0caa92da7b80302bc
+ZONE_ID="Z0732355102QE6GB8XDYY"
+DOMAIN_NAME="sgrdevsecops.fun"
 
 for instance in $@
 do
@@ -10,9 +12,29 @@ do
     #get Private IP
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" #catalogue.sgrdevsecops.fun
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     fi
 
     echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+   --hosted-zone-id $ZONE_ID \
+   --change-batch '{
+       "Comment": "updating record set",
+       "Changes": [
+           {
+               "Action": "UPSERT",
+               "ResourceRecordSet": {
+                   "Name": "'$RECORD_NAME'",
+                   "Type": "A",
+                   "TTL": 1,
+                   "ResourceRecords": [
+                       { "Value": "'$IP'" }
+                   ]
+               }
+           }
+       ]
+   }'
 done
