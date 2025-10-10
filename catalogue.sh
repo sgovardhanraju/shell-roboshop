ca075c7f2ea7c7e5843e8817d72f377aadedde08
Govardhan Raju <gsurapur@gmail.com>
2025-10-10 23:25:50 +0400

    shell

--- catalogue.sh
+++ catalogue.sh
@@ -39,8 +39,13 @@ VALIDATE $? "Enabling NodeJS:20"
 dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing NodeJS"
 
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-VALIDATE $? "Creating system user"
+id roboshop
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating system user"
+eslse
+    echo "User already exist ... $Y SKIPPING $N"
+fi
 
 mkdir /app
 VALIDATE $? "Creating app directory"
