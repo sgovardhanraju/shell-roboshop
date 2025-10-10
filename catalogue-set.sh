313c61238fc0524cc6fe5e187a20746a48c49285
Govardhan Raju <gsurapur@gmail.com>
2025-10-11 00:28:59 +0400

    shell

---
+++ catalogue-set.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -euo pipefail
+trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+MONGODB_HOST=mongodb.sgrdevsecops.fun
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_DIR=$PWD
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script starting excuted at $(date)" | tee -a $LOG_FILE
+if [ $USERID -ne 0 ]; then
+    echo "ERROR::Please run the script with root privileges"
+    exit 1
+fi
+
+dnf module disable nodejs -y &>>$LOG_FILE
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+dnf install nodejs -y &>>$LOG_FILE
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+else
+    echo -e "User already exist ... $Y SKIPPING $N"
+fi
+mkdir -p /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+cd /app
+rm -rf /app/*
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+npm install &>>$LOG_FILE
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
+systemctl daemon-reload
+systemctl enable catalogue &>>$LOG_FILE
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
+dnf install mongodb-mongosh1 -y &>>$LOG_FILE
+INDEX=$(mongosh mongodb.sgrdevsecops.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+else    
+    echo -e "Catalogue products already loaded $Y SKIPPING $N"
+fi
+systemctl restart catalogue &>>$LOG_FILE
